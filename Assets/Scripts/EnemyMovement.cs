e4bf0aa30d76bd1b480a9216fb7f4dd2fe7d92a8
Ulrik <ulrik.bergstrom@gmail.com>
2019-08-03 13:36:51 +0200

    Changed name to waypoint script and added a new pathfinder script and made a Breadth to search algoritm

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -5,28 +5,23 @@ using UnityEngine;
 
 public class EnemyMovement : MonoBehaviour
 {
-	[SerializeField] List<Block> path;
+	[SerializeField] List<Waypoint> path;
 
     // Start is called before the first frame update
     void Start()
     {
-		PrintAllWaypoints();
+		StartCoroutine(FollowPath());
     }
 
-	private void PrintAllWaypoints()
+	IEnumerator FollowPath()
 	{
-		foreach (Block wayPoint in path)
+		print("Starting Patrol:");
+		foreach (Waypoint waypoint in path)
 		{
-			print(wayPoint.name);
+			transform.position = waypoint.transform.position;
+			yield return new WaitForSeconds(1f);
+			print("Visiting " + waypoint);
 		}
+		print("Ending Patrol");
 	}
-
-	// Update is called once per frame
-	void Update()
-    {
-		//foreach(Block block in path)
-		//{
-		//	print(block.name + "," + block.transform.position);
-		//}
-    }
 }
