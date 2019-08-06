69397447445b6c7838a0dda29bfdeebb069c0594
Ulrik <ulrik.bergstrom@gmail.com>
2019-08-07 00:14:33 +0200

    Coroutine for making spawn forever

---
+++ Assets/Scripts/EnemySpawn.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawn : MonoBehaviour
+{
+	[Range (0.1f, 120f)]
+	[SerializeField] float secondsBetweenSpawns = 2f;
+	[SerializeField] EnemyMovement enemyPrefab;
+
+    void Start()
+    {
+		StartCoroutine(SpawnEnemies());
+    }
+
+	IEnumerator SpawnEnemies()
+	{
+		while (true) // Forever
+		{
+			Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+			print("Spawning");
+
+			yield return new WaitForSeconds(secondsBetweenSpawns);
+		}
+	}
+}
--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
+	// Parameters of each Tower
 	[SerializeField] Transform objectToPan;
-	[SerializeField] Transform targetEnemy;
 	[SerializeField] float attackRange = 20f;
 	[SerializeField] ParticleSystem projectileParticle;
+
+	// State of each Tower
+	Transform targetEnemy;
 
 	// Update is called once per frame
 	void Update()
     {
+		SetTargetEnemy();
 		if (targetEnemy)
 		{
 			objectToPan.LookAt(targetEnemy);
@@ -23,6 +27,34 @@ public class Tower : MonoBehaviour
 			Shoot(false);
 		}
     }
+
+	private void SetTargetEnemy()
+	{
+		var sceneEnemies = FindObjectsOfType<EnemyDamage>();
+		if (sceneEnemies.Length == 0) { return; }
+
+		Transform closestEnemy = sceneEnemies[0].transform;
+
+		foreach (EnemyDamage testEnemy in sceneEnemies)
+		{
+			closestEnemy = GetClosest(closestEnemy, testEnemy.transform);
+		}
+
+		targetEnemy = closestEnemy;
+	}
+
+	private Transform GetClosest(Transform transformA, Transform transformB)
+	{
+		var distanceToA = Vector3.Distance(transform.position, transformA.position);
+		var distanceToB = Vector3.Distance(transform.position, transformB.position);
+
+		if (distanceToA < distanceToB)
+		{
+			return transformA;
+		}
+
+		return transformB;
+	}
 
 	private void FireAtEnemy()
 	{
